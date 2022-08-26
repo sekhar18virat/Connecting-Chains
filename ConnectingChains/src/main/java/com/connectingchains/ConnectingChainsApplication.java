ba362324c8f2a7207b87aedb146e2fdb73c5b2a4
sekharvirat <sekharreddy1515@gmail.com>
2022-08-26 20:49:20 +0530

    Added connecting chains

---
+++ ConnectingChains/src/main/java/com/connectingchains/ConnectingChainsApplication.java
@@ -0,0 +1,13 @@
+package com.connectingchains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectingChainsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectingChainsApplication.class, args);
+	}
+
+}
