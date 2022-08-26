a996a6b598dc82747359bd166b8b7be6014587d5
sekharvirat <sekharreddy1515@gmail.com>
2022-08-26 20:57:53 +0530

    Created project folder

---
+++ ConnectingChains-1/src/main/java/com/connectingchains/ConnectingChains1Application.java
@@ -0,0 +1,13 @@
+package com.connectingchains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectingChains1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectingChains1Application.class, args);
+	}
+
+}
